fb1799a50b9952c583f54d1082ef964564fe14a5
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-09-20 10:06:00 -0400

    fix home page

--- src/app/CallToActionLinks/CallToActionLinks.js
+++ src/app/CallToActionLinks/CallToActionLinks.js
@@ -7,12 +7,12 @@ const CallToActionLinks = () => {
       <div className={styles.innerContainer}>
         <div className={styles.linkContainer}>
           <a href="/coming-soon" className={styles.link}>
-            Apply to our technical AI safety fellowship
+            Apply to our AI alignment fellowship
           </a>
           <a href="https://chicagoalignment.com/" className={styles.link}>
             Check out our upcoming convention
           </a>
-          <a href="#" className={styles.link}>
+          <a href="/coming-soon" className={styles.link}>
             Join our mailing list
           </a>
         </div>
