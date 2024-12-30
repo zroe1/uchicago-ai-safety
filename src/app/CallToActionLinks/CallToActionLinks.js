6ada4dc61937c4bbe91b3277ce17ee5d6e237a86
zephyroe <114773939+zroe1@users.noreply.github.com>
2024-12-30 17:06:17 -0600

    update fellowship link

--- src/app/CallToActionLinks/CallToActionLinks.js
+++ src/app/CallToActionLinks/CallToActionLinks.js
@@ -6,8 +6,10 @@ const CallToActionLinks = () => {
     <div className={styles.container}>
       <div className={styles.innerContainer}>
         <div className={styles.linkContainer}>
-          <a href="/coming-soon" className={styles.link}>
-            Apply to our AI alignment fellowship
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLScKAvi7FJEfi8zrXdGnHAdODk_G4RqYAQ2x5315Ow_4J36YFQ/viewform?usp=sf_link"
+            className={styles.link}>
+            Apply to our AI Safety Intro Fellowship
           </a>
           <a href="https://chicagoalignment.com/" className={styles.link}>
             Check out our upcoming convention
