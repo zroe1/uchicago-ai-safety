41fe4e7530a8447480160eaa52d2d74d10d9601e
zephyroe <114773939+zroe1@users.noreply.github.com>
2025-03-09 15:23:33 -0500

    update link to apply

--- src/app/ai-safety-fundamentals/page.js
+++ src/app/ai-safety-fundamentals/page.js
@@ -217,7 +217,7 @@ export default function Fellowship() {
           convergence to AI governance and critical perspectives on AI safety.
         </p>
         <a
-          href="https://docs.google.com/forms/d/e/1FAIpQLSf4O9WXV85jQKCTS046IDnHUqMbAu85p8vDUFSyncGzoQrJ9g/viewform?usp=sf_link"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSeFHozMw2Asi9qGCOkgWPXjxirIAI6Omp92MbLirEqAFYk-ew/viewform?usp=sharing"
           className={styles.actionButton}>
           Learn More & Apply
         </a>
